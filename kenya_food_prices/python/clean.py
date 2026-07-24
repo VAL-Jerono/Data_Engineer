4d5e1de9c885a5f65e169a53608ed4de2dc4e55c
Valerie Jerono <142293902+VAL-Jerono@users.noreply.github.com>
2026-07-24 10:39:21 +0300

    Revise module header in clean.py
    Updated module description and responsibilities in clean.py.

--- kenya_food_prices/python/clean.py
+++ kenya_food_prices/python/clean.py
@@ -1,6 +1,5 @@
 """
-Kenya Food Prices Data Engineering Capstone
-Author: Rene Bosire | Everything Data Bootcamp
+Kenya Food Prices Data Engineering 
 Module: clean.py — Data Transformation & Cleaning Layer
 
 Responsibilities:
